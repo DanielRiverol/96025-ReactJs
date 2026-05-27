635d3fbf23fb1485a5ff73f8b36ff3b7649552ab
Daniel Riverol <dannyriverol@hotmail.com>
2026-05-27 15:25:56 -0300

    Update file permissions for multiple files across the project to ensure executable access where necessary.

--- app-ejemplo/src/App.jsx
+++ app-ejemplo/src/App.jsx
@@ -1,16 +1,10 @@
-import Efectos from "./components/Efectos";
-import InputFocus from "./components/InputFocus";
-import AppCopy from "./components/AppCopy";
-import VideoPlayer from "./components/VideoPlayer";
+
 function App() {
   return (
     <>
       <main className='p-5'>
         <h1 className='text-3xl'>App ejemplo</h1>
-        {/* <Efectos /> */}
-        {/* <InputFocus /> */}
-        {/* <AppCopy/> */}
-        <VideoPlayer/>
+       
       </main>
     </>
   );
--- ecommerce-react/src/App.jsx
+++ ecommerce-react/src/App.jsx
@@ -16,8 +16,8 @@ function App() {
           <Sidebar></Sidebar>
           {/* Contenedor de productos */}
           <div className='h-full overflow-y-auto pr-2'>
-            {/* <ItemListContainer text='Listado de productos' /> */}
-            <ItemDetailContainer />
+            <ItemListContainer text='Listado de productos' />
+            {/* <ItemDetailContainer /> */}
            {/* <InputContainer/> */}
           </div>
         </section>
