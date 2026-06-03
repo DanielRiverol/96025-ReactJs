8d48b878d5873631951fe2c2a6236129e74926d8
Daniel Riverol <dannyriverol@hotmail.com>
2026-06-03 18:39:47 -0300

    fix: correct typos and improve code formatting in App component

--- ecommerce-react/src/App.jsx
+++ ecommerce-react/src/App.jsx
@@ -5,18 +5,18 @@ import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemdetailContainer";
-import InputContainer from "./components/Input/InputContainer";
+
 function App() {
   return (
     <>
       <main className='h-screen grid grid-rows-[auto_1fr_auto] overflow-hidden bg-base-100'>
         {/* Navbar */}
         <Navbar />
-        {/* Contendor rpimcipal */}
+        {/* Contendor principal */}
         <section className='grid grid-cols-[200px_1fr] gap-4 p-4 overflow-hidden'>
           {/* Sidebar */}
-          <Sidebar></Sidebar>
-          {/* Contenedor de productos */}
+          <Sidebar />
+          {/* Contenedor dinámico */}
           <div className='h-full overflow-y-auto pr-2'>
             {/* Rutas */}
             <Routes>
@@ -33,7 +33,8 @@ function App() {
             </Routes>
           </div>
         </section>
-        <Footer></Footer>
+        {/* Footer */}
+        <Footer />
       </main>
     </>
   );
